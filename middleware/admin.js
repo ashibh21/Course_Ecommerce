806f74b2432afc4285fb27237e3bdd1a05fe6dcf
Ashish bhatt <98805495+ashibh21@users.noreply.github.com>
2024-07-07 13:58:21 +0530

    Handeled the exceptiton

--- middleware/admin.js
+++ middleware/admin.js
@@ -7,12 +7,18 @@ function adminMiddleware(req, res, next) {
   const token = req.headers.authorization;
 
   const jwtTOken = token.split(" ")[1];
-  const decodedvalue = jwt.verify(jwtTOken, JWT_SECRET);
-  if (decodedvalue.username) {
-    next();
-  } else {
-    res.status(403).json({
-      message: "Not authenticated",
+  try {
+    const decodedvalue = jwt.verify(jwtTOken, JWT_SECRET);
+    if (decodedvalue.username) {
+      next();
+    } else {
+      res.status(403).json({
+        message: "Not authenticated",
+      });
+    }
+  } catch (e) {
+    res.json({
+      message: "Incorrect inputs",
     });
   }
 }
--- middleware/user.js
+++ middleware/user.js
@@ -6,12 +6,18 @@ function userMiddleware(req, res, next) {
   const token = req.headers.authorization;
 
   const jwtTOken = token.split(" ")[1];
-  const decodedvalue = jwt.verify(jwtTOken, JWT_SECRET);
-  if (decodedvalue.username) {
-    next();
-  } else {
-    res.status(403).json({
-      message: "Not authenticated",
+  try {
+    const decodedvalue = jwt.verify(jwtTOken, JWT_SECRET);
+    if (decodedvalue.username) {
+      next();
+    } else {
+      res.status(403).json({
+        message: "Not authenticated",
+      });
+    }
+  } catch (e) {
+    res.json({
+      message: "Incorrect inputs",
     });
   }
 }
